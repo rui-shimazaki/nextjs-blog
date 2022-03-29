cf21401483cb219a2f1a2cfff2f26b24ddc39c15
rui-shimazaki <85986945+rui-shimazaki@users.noreply.github.com>
2022-03-29 12:31:15 +0900

    [FIX]不具合修正

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/pages'
+import { getSortedPagesData } from '../lib/pages'
 import Link from 'next/link'
 import Date from '../components/date'
 
--- pages/pages/[slug].js
+++ pages/pages/[slug].js
@@ -1,27 +1,27 @@
 import Layout from '../../components/layout'
-import { getAllPostSlugs, getCustomFieldData, getPostData } from '../../lib/pages'
+import { getAllPageSlugs, getCustomFieldData, getPageData } from '../../lib/pages'
 import Head from 'next/head'
 import utilStyles from '../../styles/utils.module.css'
 
-export default function Post({ postData }) {
+export default function Page({ PageData }) {
 
-  var pc_css  = `<style>${postData.acf.page_pc_css}</style>`;
-  var sp_css  = `<style>${postData.acf.page_sp_css}</style>`;
-  var tmp_page_js = postData.acf.page_js;
+  var pc_css  = `<style>${PageData.acf.page_pc_css}</style>`;
+  var sp_css  = `<style>${PageData.acf.page_sp_css}</style>`;
+  var tmp_page_js = PageData.acf.page_js;
   var page_js = `<script>${tmp_page_js}</script>`
 
     return (
       <Layout>
       <Head>
-        <title>{postData.title.rendered}</title>
+        <title>{PageData.title.rendered}</title>
       </Head>
       <article>
-        <h1 className={utilStyles.headingXl}>{postData.title.rendered}</h1>
+        <h1 className={utilStyles.headingXl}>{PageData.title.rendered}</h1>
         {/* <div className={utilStyles.lightText}>
-          <Date dateString={postData.date.rendered} />
+          <Date dateString={PageData.date.rendered} />
         </div> */}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <div dangerouslySetInnerHTML={{ __html: postData.content.rendered }} />
+        <div dangerouslySetInnerHTML={{ __html: PageData.content.rendered }} />
         <div dangerouslySetInnerHTML={{ __html: pc_css }}></div>
         <div dangerouslySetInnerHTML={{ __html: page_js }}></div>
       </article>
@@ -30,7 +30,7 @@ export default function Post({ postData }) {
   }
 
 export async function getStaticPaths() {
-    const paths = await getAllPostSlugs()
+    const paths = await getAllPageSlugs()
     console.log('拝見');
     console.log(paths);
     return {
@@ -42,12 +42,12 @@ export async function getStaticPaths() {
   export async function getStaticProps({ params }) {
     // console.log("params");
     // console.log(params);
-    const basicData = await getPostData(params.slug);
+    const basicData = await getPageData(params.slug);
     const customFieldData = await getCustomFieldData(params.slug);
-    const postData = {...basicData, ...customFieldData};
+    const PageData = {...basicData, ...customFieldData};
     return {
       props: {
-        postData
+        PageData
       }
     }
   }
